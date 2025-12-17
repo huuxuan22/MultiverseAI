094abed3099463fd6f31b6c0713a17f99eeb8270
xuanhandsome <tranduonghuuxuan22@gmail.com>
2025-12-17 23:03:17 +0700

    setup i18n for UI and switch language

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
+import LanguageSwitcher from './components/LanguageSwitcher';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
+      <LanguageSwitcher />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t('COMMON.EDIT')} <code>src/App.js</code> {t('COMMON.SAVE')}.
         </p>
         <a
           className="App-link"
@@ -15,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('COMMON.LEARN')}
         </a>
       </header>
     </div>
